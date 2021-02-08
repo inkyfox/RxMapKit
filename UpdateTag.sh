c5e53111e3d5bccd87924186ad392acea472f016
CainLuo <350116542@qq.com>
2021-02-08 19:54:56 +0800

    Update UpdateTag.sh

--- UpdateTag.sh
+++ UpdateTag.sh
@@ -5,9 +5,4 @@ read tag
 git tag $tag
 git push origin --tags
 
-pod repo push yykjpodspec YYKJExtensionsKit.podspec --skip-import-validation
-pod repo push yykjpodspec YYKJNavigatorKit.podspec --skip-import-validation
-pod repo push yykjpodspec YYKJAppConfigKit.podspec --skip-import-validation
-pod repo push yykjpodspec YYKJServiceKit.podspec --skip-import-validation
-pod repo push yykjpodspec YYKJComponentManagerKit.podspec --skip-import-validation
-pod repo push yykjpodspec YYKJKit.podspec --skip-import-validation
+pod repo push yykjpodspec RxMapKit.podspec
