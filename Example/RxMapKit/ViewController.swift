3f61298b35e764c38cfdda196364303fefab6325
CainLuo <350116542@qq.com>
2021-02-08 19:50:15 +0800

    Update ViewController.swift

--- Example/RxMapKit/ViewController.swift
+++ Example/RxMapKit/ViewController.swift
@@ -39,6 +39,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        bindViewModel()
 
         locationButton.rx.tap
             .map { [weak self] in !self?.locationButton.isSelected ?? false }
@@ -46,97 +47,23 @@ class ViewController: UIViewController {
             .disposed(by: disposeBag)
 
         let track = locationButton.rx.observe(Bool.self, "selected")
-            .filter { $0 != nil }.map { $0! }.publish()
-        _ = track.filter { $0 }.take(1)
-            .subscribe(onNext: { [weak self] _ in self?.locationManager.requestWhenInUseAuthorization() })
+            .filter { $0 != nil }
+            .map { $0! }
+            .publish()
+        
+        _ = track.filter { $0 }
+            .take(1)
+            .subscribe(onNext: { [weak self] _ in
+                self?.locationManager.requestWhenInUseAuthorization()
+            })
+        
         _ = track.bind(to: mapView.rx.showsUserLocation.asObserver())
-        _ = track.map { $0 ? .follow : .none }.bind(to: mapView.rx.userTrackingModeToAnimate)
-        track.connect().disposed(by: disposeBag)
         
-        mapView.rx.regionWillChange.asDriver()
-            .drive(onNext: { print("Will region change: isAnimated \($0.isAnimated)") })
-            .disposed(by: disposeBag)
+        _ = track.map { $0 ? .follow : .none }
+            .bind(to: mapView.rx.userTrackingModeToAnimate)
         
-        mapView.rx.regionDidChange.asDriver()
-            .drive(onNext: { print("Did region change: \($0.region) isAnimated \($0.isAnimated)") })
+        track.connect()
             .disposed(by: disposeBag)
-        
-        mapView.rx.willStartLoadingMap.asDriver()
-            .drive(onNext: { print("Will start loading map") })
-            .disposed(by: disposeBag)
-        
-        mapView.rx.didFinishLoadingMap.asDriver()
-            .drive(onNext: { print("Did finish loading map") })
-            .disposed(by: disposeBag)
-        
-        mapView.rx.didFailLoadingMap.asDriver()
-            .drive(onNext: { print("Did fail loading map with error: \($0)") })
-            .disposed(by: disposeBag)
-        
-        mapView.rx.willStartRenderingMap.asDriver()
-            .drive(onNext: { print("Will start rendering map") })
-            .disposed(by: disposeBag)
-        
-        mapView.rx.didFinishRenderingMap.asDriver()
-            .drive(onNext: { print("Did finish rendering map: fully rendered \($0.isFullyRendered)") })
-            .disposed(by: disposeBag)
-
-        mapView.rx.didAddAnnotationViews.asDriver()
-            .drive(onNext: { views in
-                for v in views {
-                    print("Did add annotation views: \(v.annotation!.title! ?? "unknown")")
-                    v.transform = CGAffineTransform(scaleX: 0.3, y: 0.3)
-                    UIView.animate(withDuration: 0.4) { v.transform = CGAffineTransform.identity }
-                }
-            })
-            .disposed(by: disposeBag)
-        
-        mapView.rx.didSelectAnnotationView.asDriver()
-            .drive(onNext: { view in
-                print("Did selected: \(view.annotation!.title! ?? "")")
-                view.image = #imageLiteral(resourceName: "marker_selected")
-            })
-            .disposed(by: disposeBag)
-
-        mapView.rx.didDeselectAnnotationView.asDriver()
-            .drive(onNext: { view in
-                print("Did deselected: \(view.annotation!.title! ?? "")")
-                view.image = #imageLiteral(resourceName: "marker_normal")
-            })
-            .disposed(by: disposeBag)
-        
-        mapView.rx.willStartLocatingUser.asDriver()
-            .drive(onNext: { print("Will start locating user") })
-            .disposed(by: disposeBag)
-        
-        mapView.rx.didStopLocatingUser.asDriver()
-            .drive(onNext: { print("Did stop locating user") })
-            .disposed(by: disposeBag)
-        
-        //mapView.rx.didUpdateUserLocation.asDriver()
-        mapView.rx.userLocation.asDriver()
-            .drive(onNext: { print("Did update user location: \($0.location?.description ?? "")") })
-            .disposed(by: disposeBag)
-        
-        mapView.rx.showsUserLocation.asObservable()
-            .subscribe(onNext: { print("Shows user location: \($0)") })
-            .disposed(by: disposeBag)
-
-        mapView.rx.didFailToLocateUser.asDriver()
-            .drive(onNext: { print("Did fail to locate user: \($0)") })            .disposed(by: disposeBag)
-        
-        mapView.rx.dragStateOfAnnotationView.asDriver()
-            .drive(onNext: { (view, newState, oldState) in
-                print("Drag state did changed: \(view.annotation!.title! ?? "unknown"), \(newState.rawValue) <- \(oldState.rawValue)")
-            })
-            .disposed(by: disposeBag)
-        
-        mapView.rx.didAddRenderers.asDriver()
-            .drive(onNext: { renderers in
-                for r in renderers { print("Did add renderer: \(r.overlay.title! ?? "unknown")") }
-            })
-            .disposed(by: disposeBag)
-        
     }
 
     override func viewDidAppear(_ animated: Bool) {
@@ -148,8 +75,8 @@ class ViewController: UIViewController {
     }
     
     private func addMapObjects() {
-        let center = CLLocationCoordinate2D(latitude: 33.3659424, longitude: 126.3476852)
-        let place0 = CLLocationCoordinate2D(latitude: 33.4108625, longitude: 126.391319)
+        let center = CLLocationCoordinate2D(latitude: 23.16, longitude: 113.23)
+        let place0 = CLLocationCoordinate2D(latitude: 22.95, longitude: 113.36)
         
         do {
             mapView.rx.handleViewForAnnotation { (mapView, annotation) in
@@ -167,8 +94,8 @@ class ViewController: UIViewController {
                 }
             }
             mapView.addAnnotations([
-                Annotation(coordinate: center, title: "Hello, RxSwift"),
-                Annotation(coordinate: place0, title: "Hello, MapKit"),
+                Annotation(coordinate: center, title: "Guangzhou"),
+                Annotation(coordinate: place0, title: "Guangzhou - Panyu"),
                 ])
         }
         
@@ -194,8 +121,112 @@ class ViewController: UIViewController {
             .disposed(by: disposeBag)
         
         //actionButton0.rx.tap.map { .satellite }.bind(to: mapView.rx.mapType).disposed(by: disposeBag)
-        actionButton1.rx.tap.map { false }.bind(to: mapView.rx.isZoomEnabled).disposed(by: disposeBag)
+        actionButton1.rx.tap.map { false }
+            .bind(to: mapView.rx.isZoomEnabled)
+            .disposed(by: disposeBag)
     }
 
 }
 
+extension ViewController {
+    func bindViewModel() {
+        mapView.rx.regionWillChange
+            .subscribe(onNext: {
+                print("Will region change: isAnimated \($0.isAnimated)")
+            })
+            .disposed(by: disposeBag)
+        
+        mapView.rx.regionDidChange
+            .subscribe(onNext: { print("Did region change: \($0.region) isAnimated \($0.isAnimated)") })
+            .disposed(by: disposeBag)
+        
+        mapView.rx.willStartLoadingMap
+            .subscribe(onNext: { print("Will start loading map") })
+            .disposed(by: disposeBag)
+        
+        mapView.rx.didFinishLoadingMap
+            .subscribe(onNext: { print("Did finish loading map") })
+            .disposed(by: disposeBag)
+        
+        mapView.rx.didFailLoadingMap
+            .subscribe(onNext: { print("Did fail loading map with error: \($0)") })
+            .disposed(by: disposeBag)
+        
+        mapView.rx.willStartRenderingMap
+            .subscribe(onNext: { print("Will start rendering map") })
+            .disposed(by: disposeBag)
+        
+        mapView.rx.didFinishRenderingMap
+            .subscribe(onNext: { print("Did finish rendering map: fully rendered \($0.isFullyRendered)") })
+            .disposed(by: disposeBag)
+
+        mapView.rx.didAddAnnotationViews
+            .subscribe(onNext: { views in
+                for v in views {
+                    print("Did add annotation views: \(v.annotation!.title! ?? "unknown")")
+                    v.transform = CGAffineTransform(scaleX: 0.3, y: 0.3)
+                    UIView.animate(withDuration: 0.4) { v.transform = CGAffineTransform.identity }
+                }
+            })
+            .disposed(by: disposeBag)
+        
+        mapView.rx.didSelectAnnotationView
+            .subscribe(onNext: { view in
+                print("Did selected: \(view.annotation!.title! ?? "")")
+//                view.image = #imageLiteral(resourceName: "marker_selected")
+                view.backgroundColor = .purple
+            })
+            .disposed(by: disposeBag)
+
+        mapView.rx.didDeselectAnnotationView
+            .subscribe(onNext: { view in
+                print("Did deselected: \(view.annotation!.title! ?? "")")
+//                view.image = #imageLiteral(resourceName: "marker_normal")
+                view.backgroundColor = .clear
+            })
+            .disposed(by: disposeBag)
+        
+        mapView.rx.willStartLocatingUser
+            .subscribe(onNext: { print("Will start locating user") })
+            .disposed(by: disposeBag)
+        
+        mapView.rx.didStopLocatingUser.asDriver()
+            .drive(onNext: { print("Did stop locating user") })
+            .disposed(by: disposeBag)
+        
+        mapView.rx.didUpdateUserLocation
+            .subscribe(onNext: { location in
+                print("Did update user location \(location)")
+            })
+            .disposed(by: disposeBag)
+        
+        mapView.rx.userLocation
+            .subscribe(onNext: { print("Did update user location: \($0.location?.description ?? "")") })
+            .disposed(by: disposeBag)
+        
+        mapView.rx.showsUserLocation.asObservable()
+            .subscribe(onNext: { print("Shows user location: \($0)") })
+            .disposed(by: disposeBag)
+
+        mapView.rx.didFailToLocateUser.asDriver()
+            .drive(onNext: { print("Did fail to locate user: \($0)") })
+            .disposed(by: disposeBag)
+        
+        mapView.rx.didFailToLocateUser
+            .subscribe(onNext: { error in
+                print("Did fail to locate user: \(error.localizedDescription)")
+            })
+            .disposed(by: disposeBag)
+        
+        mapView.rx.dragStateOfAnnotationView
+            .subscribe(onNext: { (view, newState, oldState) in
+                print("Drag state did changed: \(view.annotation!.title! ?? "unknown"), \(newState.rawValue) <- \(oldState.rawValue)")
+            })
+            .disposed(by: disposeBag)
+        
+        mapView.rx.didAddRenderers.subscribe(onNext: { renderers in
+                for r in renderers { print("Did add renderer: \(r.overlay.title! ?? "unknown")") }
+            })
+            .disposed(by: disposeBag)
+    }
+}
